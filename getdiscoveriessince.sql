9642799fe8df75a547b64e1cf32db24151b02449
Konzertheld <Konzertheld@users.noreply.github.com>
2020-03-24 21:15:29 +0100

    make sql more readable and use placeholders

--- getdiscoveriessince.sql
+++ getdiscoveriessince.sql
@@ -1,1 +1,7 @@
-SELECT neu.artist, neu.title, datetime(neu.time, 'unixepoch', 'localtime') datum FROM (SELECT * FROM recenttracks WHERE time > 1522540800) neu LEFT JOIN (SELECT * FROM recenttracks WHERE time <= 1522540800) alt ON neu.title=alt.title AND neu.artist=alt.artist GROUP BY neu.artist, neu.title HAVING alt.title IS NULL ORDER BY neu.time ASC
+SELECT neu.artist, neu.title, datetime(neu.time, 'unixepoch', 'localtime') datum 
+FROM (SELECT * FROM recenttracks 
+WHERE time > old AND time <= newnow) neu 
+LEFT JOIN (SELECT * FROM recenttracks 
+WHERE time <= old OR time > newnow) alt ON neu.title=alt.title AND neu.artist=alt.artist 
+GROUP BY neu.artist, neu.title HAVING alt.title IS NULL 
+ORDER BY neu.time ASC
